40d142e2faa07dd96e5089befd44fa6d041f87f2
Hasish97 <ict1700107@sjp.ac.lk>
2023-04-27 12:30:57 +0530

    jnkns

--- clickup.sh
+++ clickup.sh
@@ -1,3 +1,4 @@
 set -x
-java -jar /var/lib/jenkins/workspace/test/clickup/target/ClickupDashboard-0.0.1-SNAPSHOT.jar > clickup.log &
+java -jar /var/lib/jenkins/workspace/test/clickup/target/ClickupDashboard-0.0.1-SNAPSHOT.jar &
+sleep 1
 set +x
